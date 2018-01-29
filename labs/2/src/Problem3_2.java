6675a6762f5cd70b8102ff9afe74309b19cc0fa2
Jason Carrete <jasoncarrete5@gmail.com>
2018-01-29 16:19:01 -0500

    Completed Problem 3.2

--- labs/2/src/Problem3_2.java
+++ labs/2/src/Problem3_2.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Problem3_2 {
 
 	public static void main(String[] args) {
-		
+		int n1 = (int)(System.currentTimeMillis() % 10);
+		int n2 = (int)(System.currentTimeMillis() / 7 % 10);
+		int n3 = (int)(System.currentTimeMillis() / 3 % 10);
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("What is " + n1 + " + " + n2 + " + " + n3 + "? ");
+		int answer = in.nextInt();
+		in.close();
+
+		System.out.println(n1 + " + " + n2 + " + " + n3 + " = " + answer + " is " + (n1 + n2 + n3 == answer));
 	}
 }

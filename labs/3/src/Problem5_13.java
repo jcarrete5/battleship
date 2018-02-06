0c3a5cce46d94b18641c7192721c79b3c8f3b891
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-06 17:59:38 -0500

    Completed Problem 5.13

--- labs/3/src/Problem5_13.java
+++ labs/3/src/Problem5_13.java
@@ -1,6 +1,10 @@
 public class Problem5_13 {
 
 	public static void main(String[] args) {
+		final int LIMIT = 12000;
+		int n = 0;
+		while (Math.pow(n + 1, 3) < LIMIT) { n++; }
 
+		System.out.println(n + " is the largest n such that n^3 is less than " + LIMIT);
 	}
 }

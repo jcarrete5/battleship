ee79b6cacab939643db29cae9a6ecab29349722b
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-07 20:35:01 -0500

    Improved Problem 5.16 algorithm

--- labs/3/src/Problem5_16.java
+++ labs/3/src/Problem5_16.java
@@ -9,16 +9,37 @@ public class Problem5_16 {
 		in.close();
 
 		StringBuilder builder = new StringBuilder();
+		long ms = System.currentTimeMillis();
 		int factor = 2;
 		while (n > 1) {
 			if (n % factor == 0) {
 				n /= factor;
-				builder.append(factor + " ");
+				builder.append(factor).append(' ');
 			} else {
-				factor++; // Slightly inefficient since we really want factor to be the next prime
+				factor = nextPrime(factor);
 			}
 		}
+		System.out.println("Took " + (System.currentTimeMillis() - ms) + "ms");
 		String output = builder.toString().trim().replace(" ", ", ");
 		System.out.println(output);
+	}
+
+	private static int nextPrime(int n) {
+		if (n < 2) throw new IllegalArgumentException("n must be greater than or equal to 2");
+		n = n % 2 == 0 ? n + 1 : n + 2;
+		while (!isPrime(n)) {
+			n += 2;
+		}
+		return n;
+	}
+
+	private static boolean isPrime(int n) {
+		if (n < 2) throw new IllegalArgumentException("n must be greater than or equal to 2");
+		if (n == 2) return true;
+		if (n % 2 == 0) return false;
+		for (int i = 3; i <= (int)Math.ceil(Math.sqrt(n)); i += 2) {
+			if (n % i == 0) return false;
+		}
+		return true;
 	}
 }

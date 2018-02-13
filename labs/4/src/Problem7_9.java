bf19776cf02ce8349b73bb1e2b8c0e353245dc75
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-13 18:25:56 -0500

    Completed Problem 7.9

--- labs/4/src/Problem7_9.java
+++ labs/4/src/Problem7_9.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem7_9 {
+
+	public static void main(String[] args) {
+		double[] nums = new double[10];
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter ten numbers: ");
+		for (int i = 0; i < 10; i++) {
+			nums[i] = in.nextDouble();
+		}
+		in.close();
+
+		System.out.println("The minimum number is: " + min(nums));
+	}
+
+	public static double min(double[] array) {
+		return Arrays.stream(array).min().getAsDouble();
+	}
 }

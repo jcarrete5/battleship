599ade21fd2f30ca3a997cb5791dfb0b900ea588
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-13 18:13:41 -0500

    Completed Problem 7.5

--- labs/4/src/Problem7_5.java
+++ labs/4/src/Problem7_5.java
@@ -1,2 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Problem7_5 {
+
+	public static void main(String[] args) {
+		Set<Integer> distinct = new HashSet<>();
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter ten integers: ");
+		for (int i = 0; i < 10; i++) {
+			distinct.add(in.nextInt());
+		}
+		in.close();
+
+		System.out.println("The number of distinct integers is: " + distinct.size());
+		for (int n : distinct) {
+			System.out.print(n + " ");
+		}
+		System.out.println();
+	}
 }

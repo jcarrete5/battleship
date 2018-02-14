7fbad1d874b300fc96ee1962bfdce67439ae9392
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-13 23:59:36 -0500

    Fixed problems with original implementation

--- prj/1/src/A1Paper.java
+++ prj/1/src/A1Paper.java
@@ -4,32 +4,37 @@ public class A1Paper {
 
 	public static void main(String[] args) {
 		// Pre-compute lengths (in meters) of the long sides of each type of paper
-		final double root2 = Math.sqrt(2.0);
-		final double[] longLengths = new double[29];
-		longLengths[0] = Math.pow(2, -0.75);
-		for (int i = 1; i < longLengths.length; i++) {
-			longLengths[i] = (longLengths[i - 1] / 2) * root2;
+		final double ROOT2 = Math.sqrt(2.0);
+		final double[] longSideLengths = new double[30];
+		longSideLengths[0] = Math.pow(2, -0.75);
+		for (int i = 1; i < longSideLengths.length; i++) {
+			longSideLengths[i] = (longSideLengths[i - 1] / 2) * ROOT2;
 		}
+
+		double tapeLen = 0.0;
+		double A2pct = 0.0;
+		boolean possible = false;
 
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		int[] sheetCount = new int[n - 1];  // Index 0 is number of A2 paper ...
-		for (int i = 0; i < sheetCount.length; i++) {
-			sheetCount[i] = in.nextInt();
+		for (int i = 0; i < n - 1; i++) {
+			final int numSheets = in.nextInt();
+			final double sideLength = longSideLengths[i];
+			final double pct = Math.pow(2, -i - 1);
+
+			int neededSheets = (int)((1.0 - A2pct) / pct);
+			if (neededSheets - numSheets <= 0) {
+				tapeLen += neededSheets / 2 * sideLength;
+				possible = true;
+				break;
+			}
+
+			tapeLen += neededSheets / 2 * sideLength;
+			A2pct += pct * numSheets;
 		}
 		in.close();
 
-		double tapeLen = 0;
-		for (int i = sheetCount.length - 1; i >= 0; i--) {
-			int newSheets = sheetCount[i] / 2;
-			tapeLen += longLengths[i] * newSheets;
-			sheetCount[i] -= newSheets * 2;
-			if (i > 0) {
-				sheetCount[i - 1] += newSheets;
-			}
-		}
-
-		if (sheetCount[0] == 2) {
+		if (possible) {
 			System.out.println(tapeLen);
 		} else {
 			System.out.println("impossible");

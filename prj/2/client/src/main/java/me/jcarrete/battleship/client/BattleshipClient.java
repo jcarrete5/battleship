18ecaed40da3ff3e5382ae551c8433b0ea6dd097
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-23 03:54:19 -0500

    Make fxml file for a simple, unimplemented main menu

--- prj/2/client/src/main/java/me/jcarrete/battleship/client/BattleshipClient.java
+++ prj/2/client/src/main/java/me/jcarrete/battleship/client/BattleshipClient.java
@@ -1,6 +1,9 @@
 package me.jcarrete.battleship.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BattleshipClient extends Application {
@@ -11,6 +14,9 @@ public class BattleshipClient extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(ClassLoader.getSystemResource("fxml/main_menu.fxml"));
+
+		primaryStage.setScene(new Scene(root, 800, 600));
 		primaryStage.setTitle(BuildVersion.getImplTitle() + " v" + BuildVersion.getImplVersion());
 		primaryStage.show();
 		primaryStage.centerOnScreen();

4a1eb0444333c5bbd42cdf97325589a49e4035e1
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-25 19:37:45 -0500

    Added another FileHandler for the logger

--- prj/2/server/src/main/java/me/jcarrete/battleship/server/BattleshipServer.java
+++ prj/2/server/src/main/java/me/jcarrete/battleship/server/BattleshipServer.java
@@ -9,11 +9,14 @@ import java.util.logging.*;
 
 public class BattleshipServer {
 
-	public static final Logger logger;
+	public static final Logger logger = Logger.getLogger(BattleshipServer.class.getPackage().getName());
 
-	static {
-		final int MB = 1048576;
-		logger = Logger.getLogger(BattleshipServer.class.getPackage().getName());
+	public static void main(String[] args) {
+		setupLogger();
+	}
+
+	private static void setupLogger() {
+		final int MB = 1024 * 1024;
 		logger.setUseParentHandlers(false);
 		logger.setLevel(Level.ALL);
 
@@ -24,20 +27,27 @@ public class BattleshipServer {
 		consoleHandler.setErrorManager(new ErrorManager());
 		logger.addHandler(consoleHandler);
 
-		// Setup FileHandler
+		// Setup FileHandlers
 		try {
 			new File("logs/").mkdirs();
-			FileHandler fileHandler = new FileHandler("logs/server-%g.log", 50 * MB, 3);
+			FileHandler fileHandler = new FileHandler("logs/debug-%g.log", 50 * MB, 1);
 			fileHandler.setFormatter(new LogFileFormatter());
 			fileHandler.setLevel(Level.ALL);
 			fileHandler.setErrorManager(new ErrorManager());
 			logger.addHandler(fileHandler);
 		} catch (IOException e) {
-			logger.log(Level.FINE, "Unable to setup FileHandler for logging", e);
+			logger.log(Level.CONFIG, "Unable to setup FileHandler for logging", e);
 		}
-	}
 
-	public static void main(String[] args) {
-
+		try {
+			new File("logs/").mkdirs();
+			FileHandler fileHandler = new FileHandler("logs/server-%g.log", 50 * MB, 1);
+			fileHandler.setFormatter(new LogFileFormatter());
+			fileHandler.setLevel(Level.CONFIG);
+			fileHandler.setErrorManager(new ErrorManager());
+			logger.addHandler(fileHandler);
+		} catch (IOException e) {
+			logger.log(Level.CONFIG, "Unable to setup FileHandler for logging", e);
+		}
 	}
 }

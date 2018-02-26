199b03449d53f5e2d294d6fbf7ae5e8c6e46841c
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-25 20:31:11 -0500

    Tested connection between client and server

--- prj/2/client/src/main/java/me/jcarrete/battleship/client/BattleshipClient.java
+++ prj/2/client/src/main/java/me/jcarrete/battleship/client/BattleshipClient.java
@@ -1,10 +1,16 @@
 package me.jcarrete.battleship.client;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class BattleshipClient extends Application {
 
@@ -20,5 +26,15 @@ public class BattleshipClient extends Application {
 		primaryStage.setTitle(BuildVersion.getImplTitle() + " v" + BuildVersion.getImplVersion());
 		primaryStage.show();
 		primaryStage.centerOnScreen();
+	}
+
+	@FXML
+	private void onMultiPress(ActionEvent event) {
+		try {
+			Socket socket = new Socket(InetAddress.getLocalHost(), 10000);
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- prj/2/server/src/main/java/me/jcarrete/battleship/server/BattleshipServer.java
+++ prj/2/server/src/main/java/me/jcarrete/battleship/server/BattleshipServer.java
@@ -5,19 +5,56 @@ import me.jcarrete.battleship.common.logging.LogFileFormatter;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.*;
 
 public class BattleshipServer {
 
+	public static final int PORT = 10000;
 	public static final Logger logger = Logger.getLogger(BattleshipServer.class.getPackage().getName());
 
 	public static void main(String[] args) {
 		setupLogger();
-		logger.info("Test");
+
 		try {
-			throw new Exception("Error");
-		} catch (Exception e) {
-			logger.log(Level.FINE, "Error test", e);
+			final ServerSocket serverSocket = new ServerSocket(PORT);
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				try {
+					if (!serverSocket.isClosed()) {
+						serverSocket.close();
+					}
+				} catch (IOException ex) {
+					logger.log(Level.WARNING, "Failed to close ServerSocket during shutdown", ex);
+				}
+			}));
+			serverSocket.setPerformancePreferences(1, 0, 0);
+			serverSocket.setSoTimeout(0);
+			awaitConnections(serverSocket);
+		} catch (IOException ex) {
+			logger.log(Level.SEVERE, "Failed to open ServerSocket on port " + PORT, ex);
+			System.exit(1);
+		} catch (SecurityException ex) {
+			logger.log(Level.SEVERE, "Security issue", ex);
+			System.exit(1);
+		}
+	}
+
+	private static void awaitConnections(final ServerSocket serverSocket) {
+		logger.info("Waiting for incoming connections");
+		logger.fine("ServerServer connection details: " + serverSocket.toString());
+		while (true) {
+			try {
+				Socket socket = serverSocket.accept();
+				logger.info("Received connection from " + socket.toString());
+				if (!socket.isClosed()) {
+					socket.close();
+				}
+			} catch (IOException ex) {
+				logger.log(Level.WARNING, "Failed to accept a connection", ex);
+			} catch (SecurityException ex) {
+				logger.log(Level.SEVERE, "Security issue", ex);
+			}
 		}
 	}
 

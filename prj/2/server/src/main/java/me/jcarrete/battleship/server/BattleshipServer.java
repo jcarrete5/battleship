0c001939d606313c980a2e5bf4046faab9babb0e
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-26 19:34:05 -0500

    Changed BatteshipServer log messages
    Added UncaughtExceptionHandler for BattleshipServer

--- prj/2/server/src/main/java/me/jcarrete/battleship/server/BattleshipServer.java
+++ prj/2/server/src/main/java/me/jcarrete/battleship/server/BattleshipServer.java
@@ -7,15 +7,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.logging.*;
 
 public class BattleshipServer {
 
 	public static final int PORT = 10000;
-	public static final Logger logger = Logger.getLogger(BattleshipServer.class.getPackage().getName());
+	public static final Logger LOGGER = Logger.getLogger(BattleshipServer.class.getPackage().getName());
 
 	public static void main(String[] args) {
 		setupLogger();
+		Thread.setDefaultUncaughtExceptionHandler((thread, ex) ->
+				LOGGER.log(Level.SEVERE, "Uncaught exception on " + thread.getName() + " thread", ex));
 
 		try {
 			final ServerSocket serverSocket = new ServerSocket(PORT);
@@ -25,50 +28,46 @@ public class BattleshipServer {
 						serverSocket.close();
 					}
 				} catch (IOException ex) {
-					logger.log(Level.WARNING, "Failed to close ServerSocket during shutdown", ex);
+					LOGGER.log(Level.WARNING, "Failed to close ServerSocket during shutdown", ex);
 				}
 			}));
 			serverSocket.setPerformancePreferences(1, 0, 0);
 			serverSocket.setSoTimeout(0);
+			LOGGER.fine("serverSocket connection details: " + serverSocket);
 			awaitConnections(serverSocket);
 		} catch (IOException ex) {
-			logger.log(Level.SEVERE, "Failed to open ServerSocket on port " + PORT, ex);
-			System.exit(1);
-		} catch (SecurityException ex) {
-			logger.log(Level.SEVERE, "Security issue", ex);
+			LOGGER.log(Level.SEVERE, "Failed to open ServerSocket on port " + PORT, ex);
 			System.exit(1);
 		}
 	}
 
 	private static void awaitConnections(final ServerSocket serverSocket) {
-		logger.info("Waiting for incoming connections");
-		logger.fine("ServerServer connection details: " + serverSocket.toString());
+		LOGGER.info("Waiting for incoming connections");
 		while (true) {
 			try {
 				Socket socket = serverSocket.accept();
-				logger.info("Received connection from " + socket.toString());
-				if (!socket.isClosed()) {
-					socket.close();
-				}
+				LOGGER.info("Received connection from " + socket.getInetAddress() + ":" + socket.getPort());
+				LOGGER.fine("New Socket: " + socket);
+				socket.close();
+			} catch (SocketException ex) {
+				LOGGER.log(Level.FINE, "serverSocket closed during serverSocket.accept()", ex);
 			} catch (IOException ex) {
-				logger.log(Level.WARNING, "Failed to accept a connection", ex);
-			} catch (SecurityException ex) {
-				logger.log(Level.SEVERE, "Security issue", ex);
+				LOGGER.log(Level.WARNING, "Failed to accept a connection", ex);
 			}
 		}
 	}
 
 	private static void setupLogger() {
 		final int MB = 1024 * 1024;
-		logger.setUseParentHandlers(false);
-		logger.setLevel(Level.ALL);
+		LOGGER.setUseParentHandlers(false);
+		LOGGER.setLevel(Level.ALL);
 
 		// Setup ConsoleHandler
 		ConsoleHandler consoleHandler = new ConsoleHandler();
 		consoleHandler.setFormatter(new ConsoleFormatter());
 		consoleHandler.setLevel(Level.ALL);
 		consoleHandler.setErrorManager(new ErrorManager());
-		logger.addHandler(consoleHandler);
+		LOGGER.addHandler(consoleHandler);
 
 		// Setup FileHandlers
 		try {
@@ -77,9 +76,9 @@ public class BattleshipServer {
 			fileHandler.setFormatter(new LogFileFormatter());
 			fileHandler.setLevel(Level.ALL);
 			fileHandler.setErrorManager(new ErrorManager());
-			logger.addHandler(fileHandler);
+			LOGGER.addHandler(fileHandler);
 		} catch (IOException e) {
-			logger.log(Level.CONFIG, "Unable to setup FileHandler for logging", e);
+			LOGGER.log(Level.CONFIG, "Unable to setup FileHandler 'logs/debug-%g.log'", e);
 		}
 
 		try {
@@ -88,9 +87,9 @@ public class BattleshipServer {
 			fileHandler.setFormatter(new LogFileFormatter());
 			fileHandler.setLevel(Level.CONFIG);
 			fileHandler.setErrorManager(new ErrorManager());
-			logger.addHandler(fileHandler);
+			LOGGER.addHandler(fileHandler);
 		} catch (IOException e) {
-			logger.log(Level.CONFIG, "Unable to setup FileHandler for logging", e);
+			LOGGER.log(Level.CONFIG, "Unable to setup FileHandler for 'logs/server-%g.log'", e);
 		}
 	}
 }

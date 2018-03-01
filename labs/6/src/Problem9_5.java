237b81bbab543860b0094ee61c8eecf54db4d7b4
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-28 19:12:18 -0500

    Completed Problem 9.5

--- labs/6/src/Problem9_5.java
+++ labs/6/src/Problem9_5.java
@@ -1,6 +1,19 @@
+import java.util.GregorianCalendar;
+
 public class Problem9_5 {
 
 	public static void main(String[] args) {
+		GregorianCalendar cal = new GregorianCalendar();
 
+		System.out.printf("Current year, month, and day: %s, %s, %s\n",
+				cal.get(GregorianCalendar.YEAR),
+				cal.get(GregorianCalendar.MONTH),
+				cal.get(GregorianCalendar.DAY_OF_MONTH));
+
+		cal.setTimeInMillis(1234567898765L);
+		System.out.printf("Year, month, and day 1234567898765ms passed epoch: %s, %s, %s\n",
+				cal.get(GregorianCalendar.YEAR),
+				cal.get(GregorianCalendar.MONTH),
+				cal.get(GregorianCalendar.DAY_OF_MONTH));
 	}
 }

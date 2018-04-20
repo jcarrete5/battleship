2284698e447e5c9489de30cf179a9eeb655d8ec0
Jason Carrete <jasoncarrete5@gmail.com>
2018-04-19 20:45:36 -0400

    Grid cells highlight when user mouses over them

--- prj/2/client/src/main/java/me/jcarrete/battleship/client/scene/BattleshipGrid.java
+++ prj/2/client/src/main/java/me/jcarrete/battleship/client/scene/BattleshipGrid.java
@@ -2,9 +2,12 @@ package me.jcarrete.battleship.client.scene;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
+
+import static me.jcarrete.battleship.client.BattleshipClient.LOGGER;
 
 /**
  * Represents the top and bottom grid and records information
@@ -17,16 +20,47 @@ public class BattleshipGrid extends Canvas {
 
 	private ArrayList<Ship> ships;
 	private Ship[][] cells;
+	// green = hit, red = miss, blue = mouseHover, orange = fireLocation
+	private Color[][] cellHighlights;
+	private int[] lastPos;
 
 	public BattleshipGrid() {
 		ships = new ArrayList<>(5);
 		cells = new Ship[ROWS][COLS];
+		cellHighlights = new Color[ROWS][COLS];
+		lastPos = new int[] {-1, -1};
+		setOnMouseMoved(this::onMouseMoved);
+		setOnMouseExited(this::onMouseExited);
+	}
+
+	private int[] pointToGrid(double x, double y) {
+		return new int[] {(int)(y / CELL_SIZE), (int)(x / CELL_SIZE)};
+	}
+
+	private void onMouseMoved(MouseEvent event) {
+		int[] index = pointToGrid(event.getX(), event.getY());
+		int r = index[0], c = index[1];
+		if (!(lastPos[0] == r && lastPos[1] == c)) {
+			if (lastPos[0] != -1 && lastPos[1] != -1) {
+				cellHighlights[lastPos[0]][lastPos[1]] = null;
+			}
+			cellHighlights[r][c] = Color.color(0, 0, 1, 0.5);
+			lastPos[0] = r;
+			lastPos[1] = c;
+			draw();
+		}
+	}
+
+	private void onMouseExited(MouseEvent event) {
+		if (lastPos[0] != -1 && lastPos[1] != -1) {
+			cellHighlights[lastPos[0]][lastPos[1]] = null;
+		}
+		lastPos[0] = lastPos[1] = -1;
+		draw();
 	}
 
 	public void draw() {
 		final GraphicsContext g = getGraphicsContext2D();
-		g.save();
-
 		g.clearRect(0, 0, getWidth(), getHeight());
 
 		// Draw grid
@@ -50,13 +84,22 @@ public class BattleshipGrid extends Canvas {
 			s.draw(g);
 		}
 
+		// Draw highlights
+		g.save();
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLS; col++) {
+				if (cellHighlights[row][col] == null) continue;
+				g.setFill(cellHighlights[row][col]);
+				g.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+			}
+		}
 		g.restore();
 	}
 
 	/**
 	 * Adds a ship to the grid.
 	 * @param ship
-	 * @return <tt>true</tt> if the ship was placed successfullly, otherwise <tt>false</tt>.
+	 * @return <tt>true</tt> if the ship was placed successfully, otherwise <tt>false</tt>.
 	 */
 	public boolean addShip(Ship ship) {
 		Ship[][] oldCells = new Ship[ROWS][COLS];
